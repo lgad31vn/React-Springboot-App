334b0104a3835e20baba987295cc16521c76beb9
lgad31vn <lgad31vn@gmail.com>
2022-01-07 17:55:29 -0600

    init springboot backend

---
+++ fullstack_app/src/test/java/com/fullstack/fullstack_app/FullstackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.fullstack_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
